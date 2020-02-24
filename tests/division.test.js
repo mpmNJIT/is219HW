8e0c73ee10edde44f2e84c943071738266988132
Matthew M <mpm54@njit.edu>
2020-02-24 14:40:51 -0500

    Fixed some tests

--- tests/division.test.js
+++ tests/division.test.js
@@ -1,4 +1,4 @@
-const division = require('../divide');
+const division = require('../division');
 
 test('divide 2 divided by  2 to equal 1', () => {
     expect(division.divide(2, 2)).toBe(1);
--- tests/multiplication.test.js
+++ tests/multiplication.test.js
@@ -1,4 +1,4 @@
-const multiplication = require('../multiply');
+const multiplication = require('../multiplication');
 
 test('multiplies 3 x 3 to equal 9', () => {
     expect(multiplication.multiply(3, 3)).toBe(9);
